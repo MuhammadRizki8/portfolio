eaacd2a7e2f094af0a18f826e3bf2fcea90ae282
MuhammadRizki8 <muhammadrizki@upi.edu>
2024-08-10 22:16:51 +0700

    add risk predict

--- src/data/project.js
+++ src/data/project.js
@@ -2,6 +2,8 @@ import lenderup from '../assets/projects/lenderup.png';
 import minatku from '../assets/projects/minatku.png';
 import sma13 from '../assets/projects/sma13.png';
 import donasi from '../assets/projects/donasi.png';
+import dss from '../assets/projects/dss.png';
+import creditRisk from '../assets/projects/creditRisk.png';
 
 const projects = [
   {
@@ -35,6 +37,23 @@ const projects = [
     link: 'https://github.com/MuhammadRizki8/laravel-school-profile',
     roles: ['Web Developer'],
     techStack: ['Laravel', 'MySQL'],
+  },
+  {
+    name: 'Decision Support System Website',
+    img: dss,
+    description: 'Web application that assists in decision making using the Weighted Product method. This method is used to evaluate alternatives based on several criteria that have their respective weights.',
+    link: 'https://github.com/MuhammadRizki8/weighted-product-decision-system-laravel',
+    roles: ['Web Developer'],
+    techStack: ['Laravel', 'MySQL'],
+  },
+  {
+    name: 'Credit Risk Prediction',
+    img: creditRisk,
+    description:
+      'developed a machine learning model to predict the likelihood of loan default. The primary goal was to assist lending institutions in making informed decisions when evaluating loan applications. The project included the following steps:Data Preprocessing, Feature Engineering, Model Development, Model Evaluation',
+    link: 'https://github.com/MuhammadRizki8/Credit-Risk-Prediction',
+    roles: ['Data Scientist'],
+    techStack: ['Scikit-Learn', 'Pandas', 'Matplotlib', 'Seaborn'],
   },
 ];
 
